f5096cb9a9bd16e88be72d66a65cee118ab37d41
maurocoimbra <maurocoimbrab@gmail.com>
2025-02-21 10:34:43 -0300

    final

--- client.py
+++ client.py
@@ -1,32 +1,66 @@
 from Crypto.Cipher import AES
+from Crypto.Util.Padding import unpad
 from Crypto.Util.Padding import pad
 from Crypto.Random import get_random_bytes
 import base64
 import requests
 import random
 
-SECRET_KEY = b'minhachavesegura'  # Deve ter 16 bytes
+from flask import request, jsonify
 
-def encrypt_message(message: str) -> dict:
-    iv = get_random_bytes(16)  # IV aleatório
+# Chave simétrica compartilhada (deve ser segura e igual à usada pelo servidor)
+SECRET_KEY = b'minhachavesegura'  # 16 bytes para AES-128
+
+def encrypt_message(message: str, iv: str) -> bytes:
     cipher = AES.new(SECRET_KEY, AES.MODE_CBC, iv)
     padded_message = pad(message.encode('utf-8'), AES.block_size)
     encrypted_bytes = cipher.encrypt(padded_message)
     
+    # Para embaralhar os bytes da mensagem criptografada, não deu certo
     # encrypted_bytes = bytearray(encrypted_bytes)
     # random_index = random.randint(0, len(encrypted_bytes) - 1)
     # encrypted_bytes[random_index] ^= 0xFF
 
-    return {
+    return encrypted_bytes
+
+def build_payload(encrypted_bytes, iv: str) -> dict:
+   return {
         'encrypted_message': base64.b64encode(encrypted_bytes).decode('utf-8'),
         'iv': base64.b64encode(iv).decode('utf-8')
     }
 
+def decrypt_message(encrypted_message, iv: str) -> str:
+    # Decodificar e descriptografar
+    cipher = AES.new(SECRET_KEY, AES.MODE_CBC, iv)
+    encrypted_bytes = base64.b64decode(encrypted_message)
+    decrypted_bytes = unpad(cipher.decrypt(encrypted_bytes), AES.block_size)
+
+    decrypted_message = decrypted_bytes.decode('utf-8')
+
+    return decrypted_message
+
 # Mensagem a ser enviada
-message = "Hello Cryptography"
+message = "Hello from client"
 
 # Envia para o servidor
-for i in range(10):
-  payload = encrypt_message(message)
+for i in range(1):
+  iv = get_random_bytes(16)  # IV aleatório
+
+  encrypted_message = encrypt_message(message, iv)
+  payload = build_payload(encrypted_message, iv)
   response = requests.post('http://127.0.0.1:5000/decrypt', json=payload)
-  print(response.json())
+
+  try:
+    data = response.json()
+    encrypted_message = data.get('encrypted_message')
+    iv = base64.b64decode(data.get('iv')) # Não precisaria no nosso caso, mas deixamos caso os servidor altere o iv
+
+    decrypted_message = decrypt_message(encrypted_message, iv)
+
+    print(f"\n- Pacote do Servidor\n{data}")
+    print(f"- Mensagem do Servidor Criptografada\n{encrypted_message}")
+    print(f"- Mensagem do Servidor Descriptografada\n{decrypted_message}", end="\n\n")
+
+  except Exception as e:
+    print(data)
+
--- server copy.py
+++ server copy.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad
 from Crypto.Util.Padding import pad
-from Crypto.Random import get_random_bytes
 import base64
 
 app = Flask(__name__)
@@ -10,56 +9,41 @@ app = Flask(__name__)
 # Chave simétrica compartilhada (deve ser segura e igual à usada pelo cliente)
 SECRET_KEY = b'minhachavesegura'  # 16 bytes para AES-128
 
-def encrypt_message(message: str, iv: str) -> bytes:
+def encrypt_message_server(message: str, iv: str) -> dict:
     cipher = AES.new(SECRET_KEY, AES.MODE_CBC, iv)
     padded_message = pad(message.encode('utf-8'), AES.block_size)
     encrypted_bytes = cipher.encrypt(padded_message)
     
-    # Para embaralhar os bytes da mensagem criptografada, não deu certo
     # encrypted_bytes = bytearray(encrypted_bytes)
     # random_index = random.randint(0, len(encrypted_bytes) - 1)
     # encrypted_bytes[random_index] ^= 0xFF
 
-    return encrypted_bytes
-
-def build_payload(encrypted_bytes, iv: str) -> dict:
-   return {
+    return {
         'encrypted_message': base64.b64encode(encrypted_bytes).decode('utf-8'),
         'iv': base64.b64encode(iv).decode('utf-8')
     }
 
-def decrypt_message(encrypted_message, iv: str) -> str:
-    # Decodificar e descriptografar
-    cipher = AES.new(SECRET_KEY, AES.MODE_CBC, iv)
-    encrypted_bytes = base64.b64decode(encrypted_message)
-    decrypted_bytes = unpad(cipher.decrypt(encrypted_bytes), AES.block_size)
-
-    decrypted_message = decrypted_bytes.decode('utf-8')
-
-    return decrypted_message
-
-
 @app.route('/decrypt', methods=['POST'])
-def app_run():
+def decrypt_message():
     try:
         # Dados enviados pelo cliente
         data = request.json
         encrypted_message = data.get('encrypted_message')
         iv = base64.b64decode(data.get('iv'))  # Vetor de inicialização
 
-        decrypted_message = decrypt_message(encrypted_message, iv)
+        # Decodificar e descriptografar
+        cipher = AES.new(SECRET_KEY, AES.MODE_CBC, iv)
+        encrypted_bytes = base64.b64decode(encrypted_message)
+        decrypted_bytes = unpad(cipher.decrypt(encrypted_bytes), AES.block_size)
 
-        print(f"\n- Pacote do Cliente\n{data}")
-        print(f"- Mensagem do Cliente Criptografada\n{encrypted_message}")
-        print(f"- Mensagem do Cliente Descriptografada\n{decrypted_message}", end="\n\n")
+        decrypted_message = decrypted_bytes.decode('utf-8')
 
-        return_message = "Hello from server"
-        iv = get_random_bytes(16)  # IV aleatório
-        encrypted_message = encrypt_message(return_message, iv)
-        payload = build_payload(encrypted_message, iv)
+        print(f"\nMensagem Criptografada: {encrypted_message}\nMensagem Descriptografada: {decrypted_message}\n")
 
-        return payload
-    
+        return jsonify({
+            'encrypted_message': encrypted_message,
+            'decrypted_message': decrypted_message
+        })
     except Exception as e:
         return jsonify({'error': str(e)}), 400
 
