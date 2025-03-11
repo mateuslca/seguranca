d822cb2b6ddbb614304ab8f36c9712b7878eef75
maurocoimbra <maurocoimbrab@gmail.com>
2025-03-11 09:15:19 -0300

    organizando

---
+++ seminario_2/attacker/decrypt.py
@@ -0,0 +1,58 @@
+import os
+import argparse
+import glob
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+
+# Função para descriptografar arquivos
+def decrypt_file(filepath, private_key):
+    with open(filepath, "rb") as f:
+        ciphertext = f.read()
+
+    plaintext = private_key.decrypt(
+        ciphertext,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+    # Salvar o arquivo original
+    original_filepath = filepath.replace(".enc", "")
+    with open(original_filepath, "wb") as f:
+        f.write(plaintext)
+
+    os.remove(filepath)  # Remove o arquivo criptografado
+
+# Descriptografa todos os arquivos de um diretório
+def decrypt_directory(directory, private_key):
+    for filepath in glob.glob(os.path.join(directory, "*.enc")):
+        decrypt_file(filepath, private_key)
+
+def main(args):
+  input_path = args.input
+
+  if not os.path.isdir(input_path):
+      print("Precisa ser um diretório")
+      return
+
+  # Para teste
+  if not input_path.endswith("files"):
+      print("CUIDADO")
+      return
+  
+  # Carregar a chave privada
+  with open("private_key.pem", "rb") as f:
+      private_key = serialization.load_pem_private_key(f.read(), password=None)
+
+  decrypt_directory(input_path, private_key)
+  print("Arquivos descriptografados com sucesso!")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", type=str, required=True)
+    args = parser.parse_args()
+
+    main(args)
